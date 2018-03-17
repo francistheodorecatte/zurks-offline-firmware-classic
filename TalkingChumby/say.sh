ed50febf1f9945c9517afda68bfdeda6d738da20
phineasthecat <hellomunkey333@gmail.com>
2018-03-16 21:38:55 -0400

    Documentation update/Chumby voice change. :)

--- TalkingChumby/say.sh
+++ TalkingChumby/say.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -f /tmp/output.wav
-/mnt/usb/TalkingChumby/flite_cmu_us_rms -t "$1" -o /tmp/output.wav
+/mnt/usb/TalkingChumby/flite_cmu_us_kal16 -t "$1" -o /tmp/output.wav
 /usr/bin/aplay /tmp/output.wav
 rm -f /tmp/output.wav
 
